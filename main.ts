aa90cd40e4f39b1b25020fbc909d24dd2b100235
carterobviously-creator <carterobviously@gmail.com>
2026-03-26 20:31:51 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-let mySprite: Sprite = null
-let mySprite3: Sprite = null
-let mySprite2: Sprite = null
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     for (let index = 0; index < randint(0, randint(0, 10)); index++) {
         mySprite = sprites.create(img`
@@ -44,6 +41,7 @@ function more_things () {
         3 . . . . . . . . . . . . . . 3 
         `, SpriteKind.Player)
     music.play(music.createSong(assets.song`mySong`), music.PlaybackMode.UntilDone)
+    console.log("put more things")
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     for (let index = 0; index < randint(0, randint(0, 10)); index++) {
@@ -70,6 +68,7 @@ function place_things () {
         . . . . . . . . . . . . 2 2 . . 
         `, SpriteKind.Player)
     more_things()
+    console.log("things")
 }
 info.onCountdownEnd(function () {
     mySprite = sprites.create(img`
@@ -91,4 +90,29 @@ info.onCountdownEnd(function () {
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Player)
     place_things()
+})
+let mySprite2: Sprite = null
+let mySprite3: Sprite = null
+let mySprite: Sprite = null
+let myDart = darts.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . 2 2 2 2 2 2 2 . . . . . . 
+    . . 2 . . . . . . 2 . . . . . . 
+    . 2 . . . . . . . 2 . . . . . . 
+    2 2 2 2 2 2 2 2 2 2 . . . . . . 
+    . 8 8 . . . . 8 8 . . . . . . . 
+    8 . . 8 . . 8 . . 8 . . . . . . 
+    . 8 8 . . . . 8 8 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+forever(function () {
+    myDart.x += controller.dx()
+    myDart.y += controller.dy()
 })
